35451913092adc74f2510986c5d619cc65d72b30
YouVer <122602886+YouVer@users.noreply.github.com>
2023-05-17 05:34:03 +0300

    Add files via upload

---
+++ p1.php
@@ -0,0 +1,21 @@
+<?php
+
+//project set up:
+// $title = 'idiot idea'; // название
+// $date = '2022 - 2023'; // дата
+// $teacher = 'idiot old'; // учитель - классный руководитель - научный руководитель
+// $student = 'idiot small'; // ученик
+// $href = '../projects/Бочарова Создание словаря.pdf'; // ссылка на проект: ../projects/'название проекта'.pdf
+// $success = 'none'; // block - проект пройден или зачет чето типо нипомню, none - непройденно
+
+
+//project set up:
+$title = 'Some idea'; // название
+$date = '2022 - 2023'; // дата
+$student = 'Someone Bysomeone Withsomeone'; // ученик
+$discription = 'Небольшое описание. Небольшое описание. Небольшое описание. Небольшое описание. Небольшое описание.';
+$href = '../projects/Бочарова Создание словаря.pdf'; // ссылка на проект: ../projects/'название проекта'.pdf
+$success = 'none'; // block - проект пройден или зачет чето типо нипомню, none - непройденно
+
+include '0_initial.php';
+echo "$initial";
